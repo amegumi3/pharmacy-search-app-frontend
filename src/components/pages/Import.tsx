c473eeed8b1676128df5684f0b1751a77093a1b1
Megumi <megumi@Megumi-MBP.local>
2023-02-13 13:11:49 +0900

    [add] 届出受理医療機関名簿のインポート機能を作成
    各薬局の施設基準情報を確認できるようにするため、元となるexcalデータを送れるようにしました。

--- src/components/pages/Import.tsx
+++ src/components/pages/Import.tsx
@@ -5,8 +5,11 @@ import { useImport } from "hooks/useImport";
 import { SubmitForm } from "components/organisms/SubmitForm";
 
 export const Import: VFC = () => {
-  const { getFile, getSubmit, file } = useImport();
-  const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => getFile(e);
+  const { getPharmacyFile, getReportFile, getSubmit, file } = useImport();
+  const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => getPharmacyFile(e);
+
+  const onChangeReportFile = (e: ChangeEvent<HTMLInputElement>) => getReportFile(e);
+
   const onClickSubmit = () => getSubmit();
 
   return (
@@ -15,6 +18,16 @@ export const Import: VFC = () => {
         <Stack spacing={4} py={5}>
           <Text>コード内容別医療機関一覧表を添付</Text>
           <Input type="file" accept=".xlsx" onChange={onChangeFile} />
+        </Stack>
+        <Button disabled={!file} onClick={onClickSubmit}>
+          登録
+        </Button>
+      </Stack>
+
+      <Stack px={8} py={4}>
+        <Stack spacing={4} py={5}>
+          <Text>届出受理医療機関名簿を添付</Text>
+          <Input type="file" accept=".xlsx" onChange={onChangeReportFile} />
         </Stack>
         <Button disabled={!file} onClick={onClickSubmit}>
           登録
--- src/hooks/useImport.tsx
+++ src/hooks/useImport.tsx
@@ -3,8 +3,7 @@ import { ChangeEvent, useCallback, useState } from "react";
 
 export const useImport = () => {
   const [file, setFile] = useState<File | null>(null);
-  // const [fileName, setFileName] = useState<string | null>(null);
-  const getFile = (e: ChangeEvent<HTMLInputElement>) => {
+  const getPharmacyFile = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files[0].name.includes("コード内容別一覧表")) {
       setFile(files[0]);
@@ -12,6 +11,17 @@ export const useImport = () => {
       alert("ファイルが正しくセットされませんでした。確認のうえもう一度添付し直してください");
     }
   };
+
+  const getReportFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files[0].name.includes("届出受理医療機関名簿")) {
+      setFile(files[0]);
+    } else {
+      console.log(file?.name);
+      alert("ファイルが正しくセットされませんでした。確認のうえもう一度添付し直してください");
+    }
+  };
+
   const getSubmit = useCallback(async () => {
     console.log(file);
     if (file) {
@@ -19,16 +29,23 @@ export const useImport = () => {
       formData.append("file", file);
       console.log(formData);
       try {
-        await axios.post("http://localhost:3010/api/v1/pharmacies/import", formData).then((res) => {
-          console.log(res.data);
-          alert("成功しました");
-          setFile(null);
-        });
+        if (file.name.includes("コード内容別一覧表")) {
+          await axios.post("http://localhost:3010/api/v1/pharmacies/import", formData).then((res) => {
+            console.log(res.data);
+            alert("成功しました");
+            setFile(null);
+          })} else if(file.name.includes("届出受理医療機関名簿")) {
+            await axios.post("http://localhost:3010/api/v1/pharmacies/report_import", formData).then((res) => {
+              console.log(res.data);
+              alert("成功しました");
+              setFile(null);
+            })
+          } ;
       } catch (err) {
         console.log(err);
         alert("失敗しました");
       }
     }
   }, [file]);
-  return { file, setFile, getFile, getSubmit };
+  return { file, setFile, getPharmacyFile, getSubmit, getReportFile };
 };
